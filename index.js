69d1d6c141c3cfd2fba76c9f9b184d209b463ecd
roag03 <agraharidurgesh09@gmail.com>
2023-11-22 21:24:24 +0530

    js tute

---
+++ index.js
@@ -0,0 +1,12 @@
+// console.log('Hello World');
+// let firtsName= "Rohit";
+// console.log(firtsName);
+// const interestRate=0.3;
+// interestRate=1;
+// console.log(interestRate);
+let name="Rohit";
+let age=21;
+let isApproved=false;
+let firstName=undefined;
+let lastName =null;
+console.log(lastName);
