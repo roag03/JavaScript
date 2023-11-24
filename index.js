6e3d93566761c024fed7463a06e9fc3923b1f898
roag03 <agraharidurgesh09@gmail.com>
2023-11-24 13:45:40 +0530

    js tutorial

--- index.js
+++ index.js
@@ -4,9 +4,64 @@
 // const interestRate=0.3;
 // interestRate=1;
 // console.log(interestRate);
-let name="Rohit";
-let age=21;
-let isApproved=false;
-let firstName=undefined;
-let lastName =null;
-console.log(lastName);
+// let name="Rohit";
+// let age=21;
+// let isApproved=false;
+// let firstName=undefined;
+// let lastName =null;
+// console.log(lastName);
+// function createRectangle(){
+//     return rectangle={
+//         length:
+//     }
+// }
+
+// let a =10;
+// function inc( a){
+//     a++
+// }
+// inc(a);
+// console.log(a);
+// let rectangle={
+//     length:2,
+//     breadth:4
+// };
+// for(let  key in rectangle){
+//     console.log(rectangle[key]);
+// }
+
+// OBJECT CLONING 1
+// let src={
+//     a:10,
+//     b:20,
+//     c:30
+// };
+// let dest={};
+// for(let key in src){
+//     dest[key]=src[key];
+// }
+// console.log(dest);
+// src.a++
+// console.log(dest);
+
+// OBJECT CLONING 2
+// let src={
+//     a:10,
+//     b:20,
+//     c:30
+// };
+// let src2={value:34};
+// let dest=Object.assign({},src,src2);
+// console.log(dest);
+
+// OBJECT CLONING 3
+
+// let src={
+//         a:10,
+//         b:20,
+//         c:30
+//     };
+//     let dest={...src};
+//     console.log(dest);
+//     src.a++;
+//     console.log(dest);
