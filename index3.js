a52519303cc9bfd3c527ad8544a0941494799d24
roag03 <agraharidurgesh09@gmail.com>
2023-11-29 07:14:25 +0530

    day 7 of js

---
+++ index3.js
@@ -0,0 +1,69 @@
+// let numbers=[1,3,5,6,4];
+// let number2=numbers;
+// we can use .lenth=0 to empty  an arry 
+// numbers.length=0;
+// we can use splice to empty an array 
+// numbers.splice(0,numbers.length);
+// console.log(number2);
+// console.log(numbers);
+
+// let first=[1,2,3];
+// let sec=[4,5,6];
+// let combined=first.concat(sec);
+// console.log(combined);
+// let arr=[1,2,3,5,5];
+// let sliced=arr.slice(1,4);
+// console.log(sliced);
+
+
+// let first=[1,2,3];
+// let sec=[4,5,6];
+// let combineed =[...first,'a',false ,...sec,true, 'b']
+// let another=[...combineed]
+
+// console.log(another);
+
+// iterating and array 
+
+let arr=[10,49,24,'rohit',45,34,32,];
+// for(let value of arr){
+//     console.log(value);
+// }
+// for (let i=0 ;i<arr.length;i++){
+    // console.log(arr[i]);}
+    // for (const [index,value] of arr.entries()){
+    //     if(value>30) {
+    //         continue;
+    //         }else{
+    //             console.log(`${index}: ${value}`);
+    //             }
+    //             }
+
+        
+    // for (const value of arr){
+    //     if(typeof value==='string'){
+    //         break;
+    //         } else{
+    //             console.log(value);
+    //             }
+    //             }
+
+
+
+
+    // for (const value of arr){
+    //     switch(typeof value){
+    //         case "number":
+    //             console.log("Number");
+    //             break;
+    //             case "string":
+    //                 console.log("String");
+    //                 break;
+    //                 default:
+    //                     console.log("Unknown type")
+    //                     }
+    //                     }   
+
+
+
+    
