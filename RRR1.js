ed2030777f2c332d33a804bcaa7a34c2cfecd935
roag03 <agraharidurgesh09@gmail.com>
2024-01-06 21:55:59 +0530

    js tute

---
+++ RRR1.js
@@ -0,0 +1,14 @@
+// Condiotional operator
+// let age=16;
+// let result=age>=18 ? "Adult" : "Not Adult";
+// console.log(result);
+// alert("hello")
+// let fullName=prompt("Rohit Agrahari");
+// console.log(fullName);
+
+let num= prompt("Enter the Value");
+if (num%2==0) {
+    console.log(num+"The Number is even");
+    } else{
+        console.log(num+"The number is odd");
+        };
---
+++ first.js
@@ -0,0 +1,8 @@
+// const student={
+//     fullName : 'Rohit Agrahari',
+//     age: 21,
+//     gender:'Male',
+//     rollNumber: 21,
+
+// }
+// console.log(student)
