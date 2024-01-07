fc715f402dd8c97994b2130a76c7035db9109921
roag03 <agraharidurgesh09@gmail.com>
2024-01-07 18:30:43 +0530

    Update RRR1.js

--- RRR1.js
+++ RRR1.js
@@ -1,14 +1,7 @@
-// Condiotional operator
-// let age=16;
-// let result=age>=18 ? "Adult" : "Not Adult";
-// console.log(result);
-// alert("hello")
-// let fullName=prompt("Rohit Agrahari");
-// console.log(fullName);
+// let gameNum=25;
+// let useNum=prompt("Enter the number");
+// while(useNum!=gameNum){
+//     prompt("you have enter wrong guess again")
+// }
+// console.log("congratulation you have enter right number");
 
-let num= prompt("Enter the Value");
-if (num%2==0) {
-    console.log(num+"The Number is even");
-    } else{
-        console.log(num+"The number is odd");
-        };
