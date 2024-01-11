9db6fed185a1b86d028fe54dac36c4d1073f44ba
roag03 <agraharidurgesh09@gmail.com>
2024-01-11 12:53:57 +0530

    Update RRR1.js

--- RRR1.js
+++ RRR1.js
@@ -5,3 +5,49 @@
 // }
 // console.log("congratulation you have enter right number");
 
+// problem
+// let marks= [45,56,43,4,5,];
+
+// let sum=0;
+
+// for(let value of marks){
+//     sum=sum+value;
+// }
+// console.log(`the average is ${sum/marks.length}`);
+
+// function myFunction(msg){
+// // msg is parameter in this input
+// console.log(msg)
+// }
+// myFunction("Hello World!");
+// function sum(x,y){
+//     s=x+y;
+//     return s;
+// }
+// let val=sum(3,4);
+// console.log(val);
+// const arrowMul=(a,b)=>{
+//     return a*b;
+// }
+// console.log(arrowMul(10,5));
+// function countVowel(str){
+//     let count=0;
+//     for(const char of str){
+//         if(char==="a"|| char==="e"|| char==="i" ||char==="o"|| char==="u"){
+//             count++;
+//         }
+
+//     }
+//     console.log(count);
+// }
+// const countVowel = (str) => {
+//     let count = 0
+//     for(const char str){
+//         if(char==="a"|| char==="e"|| char==="i" ||char==="o"|| char==="u"){
+//             count++;
+//         }
+
+//     }
+//     console.log(count);
+// }
+
